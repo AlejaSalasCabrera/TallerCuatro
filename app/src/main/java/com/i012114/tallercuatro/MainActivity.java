0af7aa672c914fe955f50fc174549998bd752937
alejandra <aleja.20012010@gmail.com>
2017-09-02 00:43:34 -0500

    Initial commit

---
+++ app/src/main/java/com/i012114/tallercuatro/MainActivity.java
@@ -0,0 +1,60 @@
+package com.i012114.tallercuatro;
+
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.bnv_1);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_1:
+                        UnoFragment p1= new UnoFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1,p1)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_2:
+                        DosFragment p2= new DosFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1,p2)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_3:
+                        TresFragment p3= new TresFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1,p3)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+}
